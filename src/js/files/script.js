a6defcde7de98efed41c859c6b95a2ebde650436
Danila-hub <kontaigabox@mail.ru>
2022-10-18 19:02:55 +0300

    Added modal window menu

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,21 @@
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js';
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js';
+
+const desktopMenuButton = document.querySelector('.desktop-menu__burger');
+const desktopMenu = document.querySelector('.desktop-menu__modal-menu');
+
+if (desktopMenuButton && desktopMenu) {
+  document.addEventListener('click', (e) => {
+    if (e.target == desktopMenuButton) {
+      desktopMenu.classList.toggle('_active');
+    } else if (
+      !e.target.closest('.desktop-menu__burger') &&
+      !e.target.closest('.desktop-menu__modal-menu') &&
+      desktopMenu.classList.contains('_active')
+    ) {
+      desktopMenu.classList.remove('_active');
+    }
+  });
+}
